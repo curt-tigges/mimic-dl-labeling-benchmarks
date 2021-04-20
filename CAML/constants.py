f40c968f5284262d9b6811857e0333ea1ec6a1b3
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2021-04-21 07:17:09 +0800

    Add CAML and MultiResCNN

---
+++ CAML/constants.py
@@ -0,0 +1,10 @@
+PAD_CHAR = "**PAD**"
+EMBEDDING_SIZE = 100
+MAX_LENGTH = 2500
+
+# where you want to save any models you may train
+MODEL_DIR = 'C:\\Users\\gsandoval\\Projects\\caml-mimic\\models'
+
+DATA_DIR = 'C:\\Users\\gsandoval\\Projects\\caml-mimic\\mimicdata'
+MIMIC_3_DIR = 'C:\\Users\\gsandoval\\Projects\\caml-mimic\\mimicdata\\mimic3'
+MIMIC_2_DIR = '/path/to/repo/mimicdata/mimic2'
