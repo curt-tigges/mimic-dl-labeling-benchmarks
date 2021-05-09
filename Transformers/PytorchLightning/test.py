f204efc01c18c7303a2d2545d89dbd7bd0a836c1
Chance Kim <tiratano@naver.com>
2021-05-09 17:55:05 +0900

    - Add CAML's preprocessed input data - Add test code for multilabel classification ROC Curve - Move old but not working files to the Old folder

--- Transformers/PytorchLightning/test.py
+++ Transformers/PytorchLightning/test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pickle
 
+############################################
+# Pickle save & load test
+############################################
 TEST_PICKLE_PATH = './pickle_test.pkl'
 
 arr = np.array([[0.00500028], [0.3401312]], dtype=np.float32)
@@ -10,3 +13,7 @@ with open(TEST_PICKLE_PATH, 'wb') as f:  # Should be binary mode!! w"b"
 with open(TEST_PICKLE_PATH, 'rb') as f:    # Should be binary mode!! r"b"
     org_arr = pickle.load(f)
     print(org_arr)
+
+############################################
+# Multilabel Classification ROC Curve
+############################################
